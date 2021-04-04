84e6f1f0492a30a3c0aa8d1e83306f74597ea8f6
BRIAN OTIENO <osano3204@gmail.com>
2021-04-04 16:27:57 +0300

    Add files via upload

---
+++ simpleproject.py
@@ -0,0 +1,17 @@
+ni=input("Enter first number :")
+
+n2=input("Enter second number :")
+n3=input("Enter third number :")
+n4=input("Enter fourth number :")
+n1t=int(ni)
+n2t=int(n2)
+n3t=int(n3)
+n4t=int(n4)
+
+maxify=[n1t,n2t,n3t,n4t]
+values= n1t+n2t+n3t+n4t
+print("The numbers entered are :",maxify)
+print("The total is :",values )
+
+
+
