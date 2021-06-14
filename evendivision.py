00b4e502c68c662a96552d08a3241a1df68e4d75
BRIAN OTIENO <osano3204@gmail.com>
2021-06-14 09:25:58 +0300

    Checks Two Inputs
    This python product asks the user  for input of two numbers and check if they divide evenly into each other

---
+++ evendivision.py
@@ -0,0 +1,6 @@
+num=int(input("Enter your  first number: "))
+num1= int(input("Enter the second number : "))
+if num%num1==0 :
+    print( "the numbers divide evenly into each other ")
+else:
+    print( "The numbers do not divide evenly into each other ")
